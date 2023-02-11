e597fd5ead89701dd5dc670cbb923828a58c6adb
momeemt <momiimt@gmail.com>
2023-02-11 21:29:35 +0900

    🧪 add: test functions

---
+++ tests/test_functions.nim
@@ -0,0 +1,16 @@
+import std/unittest
+
+import minim/helper
+import minim/evals
+
+suite "test functions":
+  test """
+  function add(a, b) {
+    return a + b;
+  },
+  add(1, 2) == 3
+  """:
+    check tProgram(
+      tFunc("add", @["a", "b"], tAdd(tId("a"), tId("b"))),
+      tCall("add", tInt(1), tInt(2))
+    ).evaluate() == 3
